2f6a430bf6c809da4b22bf9bc2dbe0b38fb68f5e
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-07-27 20:27:09 +0100

    Change all this to vote

--- src/main/java/org/cascadebot/cascadebot/commands/fun/VoteCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/VoteCommand.java
@@ -7,22 +7,23 @@ package org.cascadebot.cascadebot.commands.fun;
 
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.exceptions.PermissionException;
-import org.cascadebot.cascadebot.CascadeBot;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.messaging.MessageType;
 import org.cascadebot.cascadebot.messaging.MessagingObjects;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
-import org.cascadebot.cascadebot.utils.buttons.Button;
-import org.cascadebot.cascadebot.utils.buttons.ButtonGroup;
+import org.cascadebot.cascadebot.utils.votes.VoteButtonGroup;
+import org.cascadebot.cascadebot.utils.votes.VoteButtonGroupBuilder;
+import org.cascadebot.cascadebot.utils.votes.VoteMessageType;
+import org.cascadebot.cascadebot.utils.votes.VoteResult;
 
-import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VoteCommand implements ICommandMain {
+
+    public static Map<Long, VoteButtonGroup> voteMap = new HashMap<>();
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
@@ -30,41 +31,30 @@ public class VoteCommand implements ICommandMain {
             context.reply("We can only handle votes with less than 9 options!");
             return;
         }
-        int t = LocalDateTime.now().getSecond();
-        Message messages;
-        ButtonGroup buttonGroup = new ButtonGroup(sender.getUser().getIdLong(), context.getChannel().getIdLong(), context.getGuild().getIdLong());
-        StringBuilder messageBuilder = new StringBuilder();
-        for (int e = 2; e < context.getArgs().length; e++) {
-            char unicode = (char) (0x0030 + e - 1);
-            buttonGroup.addButton(new Button.UnicodeButton(unicode + "\u20E3", (runner, channel, message) -> {
-                if (!runner.equals(buttonGroup.getOwner())) {
-                    return;
-                }
-            }));
-            messageBuilder.append(unicode).append("\u20E3").append(" - ").append(context.getArg(e)).append("\n");
-
+        VoteButtonGroup voteButtonGroup = voteMap.get(context.getGuild().getIdLong());
+        VoteButtonGroupBuilder buttonGroupBuilder = new VoteButtonGroupBuilder(VoteMessageType.NUMBERS).setOptionsAmount(context.getArgs().length - 2);
+        buttonGroupBuilder.setPeriodicConsumer((results, message) -> {
+            StringBuilder resultsBuilder = new StringBuilder();
+            for (VoteResult result : results) {
+                resultsBuilder.append(result.getVote()).append(" - ").append(result.getAmount()).append(' ');
+            }
+            message.editMessage(" Vote\n" + resultsBuilder.toString() + "\nLeading with" + (Integer) results.get(0).getVote()).queue();
+        });
+        buttonGroupBuilder.setVoteFinishConsumer(voteResults -> {
+            if (voteResults.size() != 0) {
+                int number = (Integer) voteResults.get(0).getVote();
+                context.reply(String.valueOf(number));
+            }
+        });
+        VoteButtonGroup buttonGroup = buttonGroupBuilder.build(sender.getUser().getIdLong(), context.getChannel().getIdLong(), context.getGuild().getIdLong());
+        voteMap.put(context.getGuild().getIdLong(), buttonGroup);
+        EmbedBuilder embed = MessagingObjects.getMessageTypeEmbedBuilder(MessageType.INFO, sender.getUser());
+        //buttonGroupBuilder.setVoteTime()
+        embed.setTitle("Vote");
+        for (int i = 2; i != context.getArgs().length; ++i) {
+            embed.addField("Option " + String.valueOf(i - 1), context.getArg(i), false);
         }
-        EmbedBuilder embedBuilder = MessagingObjects.getMessageTypeEmbedBuilder(MessageType.INFO);
-        embedBuilder.setTitle("Vote on: " + context.getArg(0));
-        embedBuilder.setDescription(messageBuilder.toString());
-        try {
-            context.getUIMessaging().sendButtonedMessage(embedBuilder.build(), buttonGroup);
-
-        } catch (PermissionException e) {
-            context.getTypedMessaging().replyWarning("We need the permission \"add reactions\" for this");
-        }
-        while (true) {
-            if (LocalDateTime.now().getMinute() == Integer.valueOf(context.getArg(1))) {
-
-
-                context.getChannel().getMessageById(buttonGroup.getMessageId()).queue(message -> message.delete().queue());
-                context.getChannel().getMessageById(buttonGroup.getMessageId()).queue(message -> message.getReactions().size());
-
-                context.reply("The time is f*cking over!");
-                break;
-            } else continue;
-        }
-
+        context.getUIMessaging().sendButtonedMessage(embed.build(), buttonGroup);
     }
 
     @Override
